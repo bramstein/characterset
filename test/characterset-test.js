efdbf415ae568742a8ed3c5cf9afaaf8ae74d2df
Bram Stein <b.l.stein@gmail.com>
2013-02-13 16:13:47 +0100

    Initial commit.

---
+++ test/characterset-test.js
@@ -0,0 +1,214 @@
+describe('CharacterSet', function () {
+  describe('#constructor', function () {
+    it('should create a CharacterSet instance that is empty', function () {
+      var cs = new CharacterSet();
+
+      expect(cs).not.to.be(null);
+      expect(cs.data).to.be.empty();
+    });
+
+    it('should create a CharacterSet instance with a single code point', function () {
+      var cs = new CharacterSet(1);
+
+      expect(cs).not.to.be(null);
+      expect(cs.data).to.eql({1: true});
+    });
+
+    it('should create a CharacterSet instance from an ASCII string', function () {
+      var cs = new CharacterSet('abc');
+
+      expect(cs).not.to.be(null);
+      expect(cs.data).to.eql({
+        97: true,
+        98: true,
+        99: true
+      });
+    });
+
+    it('should create a CharacterSet instance from a BMP string', function () {
+      var cs = new CharacterSet('中国');
+
+      expect(cs).not.to.be(null);
+      expect(cs.data).to.eql({
+        20013: true,
+        22269: true
+      });
+    });
+
+    it('should create a CharacterSet instance from a string containing surrogate pairs', function () {
+      var cs = new CharacterSet('a\uD834\uDF06bc');
+
+      expect(cs).not.to.be(null);
+      expect(cs.data).to.eql({
+        97: true,
+        98: true,
+        99: true,
+        119558: true
+      });
+    });
+  });
+
+  describe('#toArray', function () {
+    var cs = new CharacterSet();
+
+    it('should return the correct code points', function () {
+      var cs = new CharacterSet([1, 2]);
+
+      expect(cs.toArray()).to.eql([1, 2]);
+    });
+  });
+
+  describe('#isEmpty', function () {
+    it('should return true when empty', function () {
+      var cs = new CharacterSet();
+
+      expect(cs.isEmpty()).to.be(true);
+    });
+
+    it('should return false when not empty', function () {
+      var cs = new CharacterSet(119558);
+
+      expect(cs.isEmpty()).to.be(false);
+    });
+  });
+
+  describe('#add', function () {
+    var cs = new CharacterSet();
+
+    it('should add a single code point', function () {
+      cs.add(1);
+
+      expect(cs.size).to.eql(1);
+      expect(cs.data).to.eql({
+        1: true
+      });
+    });
+
+    it('should add another code point', function () {
+      cs.add(2);
+
+      expect(cs.size).to.eql(2);
+      expect(cs.data).to.eql({
+        1: true,
+        2: true
+      });
+    });
+
+    it('should not add the same code point twice', function () {
+      cs.add(1);
+      cs.add(2);
+
+      expect(cs.size).to.eql(2);
+      expect(cs.data).to.eql({
+        1: true,
+        2: true
+      });
+    });
+  });
+
+  describe('#remove', function () {
+    var cs = new CharacterSet([1, 2, 3, 4]);
+
+    it('should remove a single code point', function () {
+      cs.remove(1);
+
+      expect(cs.size).to.eql(3);
+      expect(cs.data).to.eql({
+        1: false,
+        2: true,
+        3: true,
+        4: true
+      });
+    });
+
+    it('should remove another code point', function () {
+      cs.remove(2);
+
+      expect(cs.size).to.eql(2);
+      expect(cs.data).to.eql({
+        1: false,
+        2: false,
+        3: true,
+        4: true
+      });
+    });
+
+    it('should not remove the same code point twice', function () {
+      cs.remove(1);
+      cs.remove(2);
+
+      expect(cs.size).to.eql(2);
+      expect(cs.data).to.eql({
+        1: false,
+        2: false,
+        3: true,
+        4: true
+      });
+    });
+  });
+
+  describe('encodeCodePoint', function () {
+    it('should encode ASCII safe characters as themselves', function () {
+      expect(CharacterSet.encodeCodePoint(65)).to.eql('A');
+      expect(CharacterSet.encodeCodePoint(57)).to.eql('9');
+      expect(CharacterSet.encodeCodePoint(97)).to.eql('a');
+    });
+
+    it('should encode ASCII unsafe code points encoded', function () {
+      expect(CharacterSet.encodeCodePoint(0)).to.eql('\\u0000');
+      expect(CharacterSet.encodeCodePoint(36)).to.eql('\\u0024');
+      expect(CharacterSet.encodeCodePoint(62)).to.eql('\\u003E');
+      expect(CharacterSet.encodeCodePoint(127)).to.eql('\\u007F');
+    });
+
+    it('should always encode code points in the BMP that are not safe characters', function () {
+      expect(CharacterSet.encodeCodePoint(20013)).to.eql('\\u4E2D');
+      expect(CharacterSet.encodeCodePoint(22269)).to.eql('\\u56FD');
+    });
+
+    it('should encode code points outside the BMP as surrogate pairs', function () {
+      expect(CharacterSet.encodeCodePoint(119558)).to.eql('\\uD834\\uDF06');
+    });
+  });
+
+  describe('#toString', function () {
+    it('should return ASCII as ASCII', function () {
+      var cs = new CharacterSet('abc');
+
+      expect(cs.toString()).to.eql('abc');
+    });
+
+    it('should not contain duplicates', function () {
+      var cs = new CharacterSet('abcabc');
+
+      expect(cs.toString()).to.eql('abc');
+    });
+
+    it('should encode characters inside the BMP as using hex escapes', function () {
+      var cs = new CharacterSet([20013, 22269]);
+
+      expect(cs.toString()).to.eql('\\u4E2D\\u56FD');
+    });
+
+    it('should encode characters outside the BMP as surrogate pairs', function () {
+      var cs = new CharacterSet(119558);
+
+      expect(cs.toString()).to.eql('\\uD834\\uDF06');
+    });
+  });
+
+  describe('#union', function () {
+  });
+
+  describe('#intersection', function () {
+  });
+
+  describe('#difference', function () {
+  });
+
+  describe('#subset', function () {
+  });
+
+  describe('#toRegExp', function () {
+  });
+});
