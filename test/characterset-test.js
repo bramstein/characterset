d9bf8ef7e709494f05bd6fb6f0fe05f6c4a974a9
Angel <angelcabo3@gmail.com>
2017-03-21 15:33:39 -0400

    Add ability to add a parsed unicode range string to the character set

--- test/characterset-test.js
+++ test/characterset-test.js
@@ -685,4 +685,88 @@ describe('CharacterSet', function () {
       expect(cs.toRegExp()).to.eql('\\uD834[\\uDF06-\\uDFFF]|[\\uD835-\\uD83B][\\uDC00-\\uDFFF]|\\uD83C[\\uDC00-\\uDC04]');
     });
   });
+
+  describe('parseUnicodeRange', function () {
+    var cs = null;
+
+    beforeEach(function () {
+      cs = new CharacterSet();
+    });
+
+    it('parses an empty string', function () {
+      cs.parseUnicodeRange('');
+
+      expect(cs.size).to.eql(0);
+      expect(cs.data).to.eql({});
+    });
+
+    it('parses a single value', function () {
+      cs.parseUnicodeRange('u+23,U+23');
+
+      expect(cs.size).to.eql(1);
+      expect(cs.data).to.eql({
+        35: true
+      });
+    });
+
+    it('parses multiple values', function () {
+      cs.parseUnicodeRange('u+23, u+22');
+      cs.parseUnicodeRange('u+23,u+22');
+
+      expect(cs.size).to.eql(2);
+      expect(cs.data).to.eql({
+        34: true,
+        35: true
+      });
+    });
+
+    it('parses ranges', function () {
+      cs.parseUnicodeRange('u+22-25');
+
+      expect(cs.size).to.eql(4);
+      expect(cs.data).to.eql({
+        34: true,
+        35: true,
+        36: true,
+        37: true
+      });
+    });
+
+    it('parses multiple ranges', function () {
+      cs.parseUnicodeRange('u+22-24, u+25-28');
+
+      expect(cs.data).to.eql({
+        34: true,
+        35: true,
+        36: true,
+        37: true,
+        38: true,
+        39: true,
+        40: true
+      });
+    });
+
+    it('parses wildcards', function () {
+      cs.parseUnicodeRange('u+1?');
+
+      expect(cs.data).to.eql({
+        16: true,
+        17: true,
+        18: true,
+        19: true,
+        20: true,
+        21: true,
+        22: true,
+        23: true,
+        24: true,
+        25: true,
+        26: true,
+        27: true,
+        28: true,
+        29: true,
+        30: true,
+        31: true
+      });
+    });
+  });
 });
