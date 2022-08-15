442e3076d552731ce263d6e00cbc2eaeb09ffa7b
Bram Stein <b.l.stein@gmail.com>
2022-08-15 12:37:02 +0200

    Convert to ES6 module.

--- test/characterset-test.js
+++ test/characterset-test.js
@@ -1,15 +1,7 @@
+import CharacterSet from '../lib/characterset.js';
+import expect from 'expect.js';
+
 describe('CharacterSet', function () {
-  var CharacterSet = null,
-      expect = null;
-
-  if (typeof exports === 'object') {
-    CharacterSet = require('../lib/characterset');
-    expect = require('expect.js');
-  } else {
-    CharacterSet = window.CharacterSet;
-    expect = window.expect;
-  }
-
   describe('#constructor', function () {
     it('should create a CharacterSet instance that is empty', function () {
       var cs = new CharacterSet();
